b586a0eaa4c7098cf1fc32226b55166ff27b3525
nicosantarin <nicosantarin@hotmail.com>
2018-02-06 04:12:39 -0800

    Revert "adjusted some code"
    This reverts commit 581e3c335ceab710ed6ff06b0a6c3bcb27a3e732.

--- src/components/realtime.jsx
+++ src/components/realtime.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+<<<<<<< HEAD
 import axios from 'axios'
 
 //import rtDisplay component
 export default class Realtime extends React.Component {
   render() {
+=======
+export default function Realtime(props) {
+>>>>>>> parent of 581e3c3... adjusted some code
   return(
     <div className='Home'>
 <h1>Real Time Data Update Page</h1>
